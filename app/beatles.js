6f1bba91ea9b64468581d452afe2efe0c5e8f3ed
Zachary Rose <zachary.f.rose@gmail.com>
2016-02-16 14:29:37 -0700

    Separated out components for ui router

--- app/beatles.js
+++ app/beatles.js
@@ -1,51 +1,61 @@
-var Beatles = angular.module("Beatles", []);
+var Beatles = angular.module("beatles", [
+  'ui.router'
+]).config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+    .state('main', {
+      url: "/",
+      templateUrl: "app/main/main.tmpl.html"
+    })
+    .state('store', {
+      url: "/store",
+      templateUrl: "app/store/store.tmpl.html",
+      controller: 'BeatlesControl'
+    })
+    .state('cart', {
+      url: "/store",
+      templateUrl: "app/cart/cart.tmpl.html"
+    })
 
-Beatles.controller('BeatlesControl', function($scope, $http) {
-
-  $scope.merchtypes = [
-      {"name": 'Shirts', "type": "Shirt"},
-      {"name": 'Vinyl', "type": "Vinyl"},
-      {"name": 'CDs', "type": "CD"},
-      {"name": 'Patches', "type": "Patch"}
-  ];
-
+    /*.state('state2.list', {
+      url: "/list",
+        templateUrl: "partials/state2.list.html",
+        controller: function($scope) {
+          $scope.things = ["A", "Set", "Of", "Things"];
+        }
+      })*/
 
 
-  $http.get('data.json')
+    });
+
+    Beatles.controller('BeatlesControl', function($scope, $http) {
+      //var beatlesCtrl = this;
+
+      $scope.merchtypes = [
+          {"name": 'Shirts', "type": "Shirt"},
+          {"name": 'Vinyl', "type": "Vinyl"},
+          {"name": 'CDs', "type": "CD"},
+          {"name": 'Patches', "type": "Patch"}
+      ];
+
+      $http.get('products.json')
        .then(function(res){
           $scope.products = res.data;
           console.log($scope.products);
 
+          $scope.currentCategory = "Shirt";
 
+          function isCurrentCategory(category) {
+              return $scope.currentCategory !== null && category === $scope.currentCategory;
+          }
 
-$scope.currentCategory = "Shirt";
+          function setCurrentCategory(category) {
+              $scope.currentCategory = category;
+          }
 
-function isCurrentCategory(category) {
-    return $scope.currentCategory !== null && category === $scope.currentCategory;
-}
+          $scope.isCurrentCategory = isCurrentCategory;
+          $scope.setCurrentCategory = setCurrentCategory;
 
-function setCurrentCategory(category) {
-    $scope.currentCategory = category;
-}
-
-$scope.isCurrentCategory = isCurrentCategory;
-$scope.setCurrentCategory = setCurrentCategory;
-
-/*
-
-
-          angular.forEach($scope.products, function(value, key) {
-             value.is_soldout = (value.is_soldout==="1");
-           });
-
-          console.log($scope.products);
-*/
         });
 
-
-
-
-
-
-
-});
+    });

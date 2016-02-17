202db5c04df28f8a2280cecbbb4f0d17fc3a291a
Zachary Rose <zachary.f.rose@gmail.com>
2016-02-16 17:46:09 -0700

    Added addToCart functionality

--- app/beatles.js
+++ app/beatles.js
@@ -11,16 +11,16 @@ var Beatles = angular.module("beatles", [
     .state('store', {
       url: "/store/:type",
       templateUrl: "app/store/store.tmpl.html",
-      controller: 'BeatlesControl'
+      controller: 'StoreControl'
     })
     .state('cart', {
       url: "/store",
-      templateUrl: "app/cart/cart.tmpl.html"
+      templateUrl: "app/cart/cart.tmpl.html",
+      controller: 'StoreControl'
     })
 });
 
-Beatles.controller('BeatlesControl', function($scope, $state, $http, $stateParams) {
-  //var beatlesCtrl = this;
+Beatles.controller('StoreControl', function($scope, $state, $http, $stateParams) {
 
   var defaultCategory = "Shirt";
 
@@ -38,7 +38,6 @@ Beatles.controller('BeatlesControl', function($scope, $state, $http, $stateParam
 
   $scope.isCurrentCategory = isCurrentCategory;
 
-
   $scope.merchtypes = [
       {"name": 'Shirts', "type": "Shirt"},
       {"name": 'Vinyl', "type": "Vinyl"},
@@ -46,12 +45,57 @@ Beatles.controller('BeatlesControl', function($scope, $state, $http, $stateParam
       {"name": 'Patches', "type": "Patch"}
   ];
 
-
+  $scope.invoice = [];
 
   $http.get('products.json')
    .then(function(res){
       $scope.products = res.data;
-      console.log($scope.products);
+      //console.log($scope.products);
     });
+
+    function itemInCart(item_id, option)
+    {
+      angular.forEach($scope.invoice, function(item, key) {
+        if (item.id == item_id && item.option == option) return true;
+      });
+      return false;
+    }
+
+    function itemIncrement(item_id, option)
+    {
+      for (var i=0; i<$scope.invoice.length; i++)
+       {
+        if ($scope.invoice[i].id == item_id && $scope.invoice[i].option == option){
+          $scope.invoice[i].qty++;
+          break;
+        }
+      }
+    }
+
+    function addToCart(product, selectedoption)
+    {
+      if (itemInCart(product.id, selectedoption) )
+      {
+        itemIncrement(product.id, selectedoption)
+      }
+      else
+      {
+        $scope.invoice.push({
+          id: product.id,
+          name: product.info_title,
+          type: product.info_type,
+          price: product.info_price,
+          option: selectedoption,
+          qty: 1
+        });
+      }
+
+      console.log($scope.invoice);
+    }
+
+
+
+    $scope.addToCart = addToCart;
+
 
 });
